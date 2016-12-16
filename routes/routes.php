7db878f5d919f33da9d1d680e39ab6492a8ab06f
it.me <xuckdejiyi@163.com>
2016-12-16 11:49:01 +0800

    add routes.php  info

--- routes/routes.php
+++ routes/routes.php
@@ -1,41 +1,29 @@
 <?php
-
-/*
-|--------------------------------------------------------------------------
+/**
 | Application Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register all of the routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
-|
-*/
-
-$app->get('/', function () use ($app) {
-    return $app->version();
+ * @var $app \Laravel\Lumen\Application
+ *
+ * group 中的属性包含   middleware  prefix  suffix  namespace
+ * 在使用中间件时候，多个中间件用 | 分割
+ */
+$app->group(['namespace'=>'App\Http\Controllers'],function ()use($app){
+    $app->get('/', function () use ($app) {
+        return $app->version();
+    });
+    $app->get('/x',['as'=>'x',function(){
+        return $_GET['id'];
+    }]);
+    $app->get('/api',['uses'=>'IndexController@api']);
+    $app->get('/o/{id:[0-9]{1,}}',['uses'=>'ExampleController@xx','as'=>'xx']);
+    $app->get('/debug',function()use($app){
+        dump($app);
+    });
+    $app->get('{type:test|xx}/{id:[1-9]{1,}}',['as'=>'xx',function ($id) use ($app){
+        return route('x',['id'=>md5($id)]);
+    }]);
 });
-$app->get('/x','ExampleController@index');
-$app->get('/xxx','ExampleController@xxx');
-$app->get('/o/{id:[0-9]{1,}}',['uses'=>'ExampleController@xx','as'=>'xx']);
-$app->get('/oo','ExampleController@xxx');
-$app->get('{type:test|xx}',function () use ($app){
-//    $app->extend('index',function (){
-//        echo "xxx";
-//    });
-//    $app->extend('index',function (){
-//        echo "0000";
-//    });
-//    $index = $app->make('index',['age'=>18,'name'=>'xck']);
-//    dump($app);
-//    dump($app->resolved('index'));
-
-    dump($app->alias(mysqli::class,'xoxoxoxoxmmysql'));
-
-    $mysqli = $app->make('xoxoxoxoxmmysql',['host'=>'127.0.0.1','user'=>'root','password'=>'','database'=>'nodeapi','port'=>3306,'socket'=>'']);
-
-    $result = $mysqli->query("select * from origin_api");
-    dump($result->fetch_array());
-
-});
-
-
